2819ed119ddfb860a23e4e0cd83a641fd5b867a5
unknown <Jamal Roberts>
2020-05-10 23:02:19 -0400

    edits

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -22,31 +22,40 @@ class Profile extends React.Component{
     console.log(process.env.REACT_APP_BACK_END_URL + url );
     fetch(process.env.REACT_APP_BACK_END_URL + url )
     .then(response => response.json())
-    .then(data => this.setState({apiResponse : data}));
+    .then(data => this.setState({apiResponse : data}))
+    .catch(err => {
+      this.setState({apiResponse : ""})
+    });
   }
 
   
   render() {
 
     const currentRoute= process.env.REACT_APP_FRONT_END_URL + this.props.location.pathname;
-    return (
-      <div className="Profile">
-        <MetaTags>
-          <meta property="og:title" content={this.state.apiResponse.fullName} />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content={currentRoute} />
-          <meta property="og:image" content={process.env.REACT_APP_BACK_END_URL + "/images/" + this.state.apiResponse.profileImaagePath } />
-        </MetaTags>
-        <body>
-          <img id="profilePhoto" src ={process.env.REACT_APP_BACK_END_URL + "/images/" + this.state.apiResponse.profileImaagePath } />
-          <div class="details">
-            <h1>{this.state.apiResponse.fullName}</h1>
-            <h2>Biography</h2>
-            <p>{this.state.apiResponse.bio}</p>
-          </div>
-        </body>
-      </div>
-    );
+    if(typeof this.state.apiResponse.fullName !== 'undefined'){
+      return (
+        <div className="Profile">
+          <MetaTags>
+            <meta property="og:title" content={this.state.apiResponse.fullName} />
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content={currentRoute} />
+            <meta property="og:image" content={process.env.REACT_APP_BACK_END_URL + "/images/" + this.state.apiResponse.profileImaagePath } />
+          </MetaTags>
+          <body>
+            <img id="profilePhoto" src ={process.env.REACT_APP_BACK_END_URL + "/images/" + this.state.apiResponse.profileImaagePath } />
+            <div class="details">
+              <h1>{this.state.apiResponse.fullName}</h1>
+              <h2>Biography</h2>
+              <p>{this.state.apiResponse.bio}</p>
+            </div>
+          </body>
+        </div>
+      );
+    }else{
+      return(
+        <p>error</p>
+      )
+    }
   }
 }
 export default Profile;
